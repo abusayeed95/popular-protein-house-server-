222029daba5efc88e3f68ac61304cbec37cfb876
ahmedmusa995 <sayeedsayem8@gmail.com>
2020-12-15 15:39:08 +0600

    working

--- index.js
+++ index.js
@@ -15,67 +15,6 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-const fakeProducts = [
-  {
-    name: "Chicken Vaccine",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, hic ratione. Quam quibusdam similique perspiciatis?",
-    id: 11,
-    price: 300,
-    availability: true,
-    img:
-      "https://zootecnicainternational.com/wp-content/uploads/2020/02/chicks_and_ampule3-New-696x464.jpg",
-  },
-  {
-    name: "Cow Vaccine",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, hic ratione. Quam quibusdam similique perspiciatis?",
-    id: 22,
-    price: 300,
-    availability: false,
-    img: "https://www.anbvet.co.za/assets/images/cattle%20vaccine.jpg",
-  },
-  {
-    name: "Chicken Medicine",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, hic ratione. Quam quibusdam similique perspiciatis?",
-    id: 33,
-    price: 300,
-    availability: true,
-    img:
-      "https://s3.amazonaws.com/newhobbyfarms.com/chicken_injection_vaccine-600x347.jpg",
-  },
-  {
-    name: "Cow Medicine",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, hic ratione. Quam quibusdam similique perspiciatis?",
-    id: 44,
-    price: 300,
-    availability: true,
-    img: "https://www.snopes.com/tachyon/2020/03/GettyImages-700711951.jpg",
-  },
-  {
-    name: "Goat Vaccine",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, hic ratione. Quam quibusdam similique perspiciatis?",
-    id: 55,
-    price: 300,
-    availability: true,
-    img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcorxTGKF8mjsFWy24t0G84WcD6buXu9GjGg&usqp=CAU",
-  },
-  {
-    name: "Goat Medicine",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, hic ratione. Quam quibusdam similique perspiciatis?",
-    id: 66,
-    price: 300,
-    availability: true,
-    img:
-      "https://i1.wp.com/katiesuescountrylife.com/wp-content/uploads/2020/05/vaccine.png?fit=1024%2C549&ssl=1",
-  },
-];
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
